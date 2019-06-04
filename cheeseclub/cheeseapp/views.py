4c211fde6e3bc1d1985c143caf833a786ba09b6a
ejmcreates <44686503+ejmcreates@users.noreply.github.com>
2019-06-04 11:31:03 -0700

    creates cheese types view

--- cheeseclub/cheeseapp/views.py
+++ cheeseclub/cheeseapp/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from .models import Cheese, CheeseType, Review, Resource
 
 # Create your views here.
 def index(request):
     return render(request, 'cheeseapp/index.html')
+
+# Cheese types view
+def gettypes(request):
+    type_list=CheeseType.objects.all()
+    return render(request, 'cheeseapp/types.html', {'type_list': type_list})
+
